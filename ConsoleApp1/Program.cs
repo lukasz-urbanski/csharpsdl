3a51f741dbd7532d601357e172afd44d5e1d3bda
Lukasz Urbanski <lukasz.urbanski@yahoo.pl>
2019-06-09 15:25:15 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/*
+			for (int i = -10; i <= 0; i++)
+			{
+				Console.Write(i);
+				if (i % 2 == 0)
+				{
+					Console.Write("\tis even");
+				}
+				Console.Write("\n");
+			}
+			*/
+
+			Punkt punkt1 = new Punkt();
+			punkt1.x = 1;
+			punkt1.y = 2;
+
+			ChangePunkt(punkt1);
+			Console.ReadKey();
+		}
+		public static void ChangePunkt(Punkt p)
+		{
+			p.x = -1;
+			p.y = -1;
+		}
+		public struct Punkt
+		{
+			public int x;
+			public int y;
+		}
+	}
+}
